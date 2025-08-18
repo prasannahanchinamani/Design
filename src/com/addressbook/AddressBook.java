032839cd404f91e02537580ac7bb980d06996015
Prasanna <prasannhanchinamani.15@gmail.com>
2025-08-18 22:31:59 +0530

    UC2: Added AddressBook class and console input to add Contact

---
+++ src/com/addressbook/AddressBook.java
@@ -0,0 +1,15 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    List<Contacts> contacts;
+
+     public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+    public  void addContact(Contacts contact){
+         contacts.add(contact);
+    }
+}
--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,7 +1,44 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("WelCome to Address Book!....");
+
+
+        AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Contact Details:");
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+
+        System.out.print("Zip: ");
+        String zip = scanner.nextLine();
+
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        addressBook.addContact(contact);
+
+        System.out.println("Contact added:");
+        System.out.println(contact);
     }
 }
